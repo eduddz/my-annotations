b5cd4a9b5346661fe9174f4eb09bd95ea81bbdda
eduddz <esteves-dorta@hotmail.com>
2022-10-10 22:13:32 -0300

    Versão 1.1.3

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -6,6 +6,6 @@ export const connection = createPool({
     host: process.env.REACT_APP_HOST,
     user: process.env.REACT_APP_USER,
     password: process.env.REACT_APP_PASSWORD,
-    port: process.env.REACT_APP_PORT_DB,
+    port: parseInt(`${process.env.REACT_APP_PORT}`),
     database: process.env.REACT_APP_DATABASE,
 });
