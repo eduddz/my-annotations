be21485f5e8f575f84a077ce2388175d6007fdad
eduddz <esteves-dorta@hotmail.com>
2022-10-12 16:41:22 -0300

    Versão 1.3.7

--- server/dist/server.js
+++ server/dist/server.js
@@ -14,7 +14,7 @@ server.use((0, cors_1.default)());
 // => USER
 server.post("/add-user", async (req, res) => {
     const { name, email, password } = req.body;
-    const query_user = "INSERT INTO `my_annotations`.`my_user` (name, email, password) VALUES (?, ?, ?)";
+    const query_user = "INSERT INTO my_user (name, email, password) VALUES (?, ?, ?)";
     const new_user = await connect.query(query_user, [name, email, password]);
     res.json(new_user[0]);
 });
@@ -43,13 +43,13 @@ server.post("/my-tasks", async (req, res) => {
 });
 server.patch("/patch-tasks-task", async (req, res) => {
     const { id_tasks, task } = req.body;
-    const query_task = "UPDATE `my_annotations`.`my_task` SET task = ? WHERE id_tasks = ?";
+    const query_task = "UPDATE my_task SET task = ? WHERE id_tasks = ?";
     const put_task = await connect.query(query_task, [task, id_tasks]);
     res.json(put_task[0]);
 });
 server.patch("/patch-tasks-clicked", async (req, res) => {
     const { id_tasks, clicked } = req.body;
-    const query_task = "UPDATE `my_annotations`.`my_task` SET clicked = ? WHERE id_tasks = ?";
+    const query_task = "UPDATE my_task SET clicked = ? WHERE id_tasks = ?";
     const put_task = await connect.query(query_task, [clicked, id_tasks]);
     res.json(put_task[0]);
 });
@@ -59,14 +59,17 @@ server.delete("/delete-task", async (req, res) => {
     const task = await connect.query(query_delete, [id_tasks]);
     res.json(task);
 });
+server.get("/teste", async (req, res) => {
+    res.json({ "message": "oi" });
+});
 // => CREATE TABLES
 server.get("/create-task-table", async (req, res) => {
-    const query_table = "CREATE TABLE IF NOT EXISTS `my_annotations`.`my_task` (`id_tasks` INT NOT NULL AUTO_INCREMENT, `task` VARCHAR(300), `color` VARCHAR(7), `clicked` TINYINT, `id_users` VARCHAR(9), PRIMARY KEY (`id_tasks`))";
+    const query_table = "CREATE TABLE IF NOT EXISTS my_task (`id_tasks` INT NOT NULL AUTO_INCREMENT, `task` VARCHAR(300), `color` VARCHAR(7), `clicked` TINYINT, `id_users` VARCHAR(9), PRIMARY KEY (`id_tasks`))";
     const created = await connect.query(query_table);
     res.json(created);
 });
 server.get("/create-user-table", async (req, res) => {
-    const query_table = "CREATE TABLE IF NOT EXISTS `my_annotations`.`my_user` (`id_users` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(50), `email` VARCHAR(25), `password` VARCHAR(8), PRIMARY KEY (`id_users`))";
+    const query_table = "CREATE TABLE IF NOT EXISTS my_user (`id_users` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(50), `email` VARCHAR(25), `password` VARCHAR(8), PRIMARY KEY (`id_users`))";
     const created = await connect.query(query_table);
     res.json(created);
 });
--- server/src/server.ts
+++ server/src/server.ts
@@ -16,7 +16,7 @@ server.use(cors());
 server.post("/add-user", async(req: Request, res: Response) => {
     const { name, email, password } = req.body;
 
-    const query_user = "INSERT INTO `my_annotations`.`my_user` (name, email, password) VALUES (?, ?, ?)";
+    const query_user = "INSERT INTO my_user (name, email, password) VALUES (?, ?, ?)";
     const new_user = await connect.query(query_user, [name, email, password]);
 
     res.json(new_user[0]);
@@ -58,7 +58,7 @@ server.post("/my-tasks", async(req: Request, res: Response) => {
 server.patch("/patch-tasks-task", async(req: Request, res: Response) => {
     const { id_tasks, task } = req.body;
 
-    const query_task = "UPDATE `my_annotations`.`my_task` SET task = ? WHERE id_tasks = ?";
+    const query_task = "UPDATE my_task SET task = ? WHERE id_tasks = ?";
     const put_task = await connect.query(query_task, [task, id_tasks]);
 
     res.json(put_task[0]);
@@ -67,7 +67,7 @@ server.patch("/patch-tasks-task", async(req: Request, res: Response) => {
 server.patch("/patch-tasks-clicked", async(req: Request, res: Response) => {
     const { id_tasks, clicked } = req.body;
 
-    const query_task = "UPDATE `my_annotations`.`my_task` SET clicked = ? WHERE id_tasks = ?";
+    const query_task = "UPDATE my_task SET clicked = ? WHERE id_tasks = ?";
     const put_task = await connect.query(query_task, [clicked, id_tasks]);
 
     res.json(put_task[0]);
@@ -81,18 +81,22 @@ server.delete("/delete-task", async(req: Request, res: Response) => {
     res.json(task);
 });
 
+server.get("/teste", async (req: Request, res: Response) => {
+    res.json({"message": "oi"});
+})
+
 
 // => CREATE TABLES
 
 server.get("/create-task-table", async (req: Request, res: Response) => {
-    const query_table = "CREATE TABLE IF NOT EXISTS `my_annotations`.`my_task` (`id_tasks` INT NOT NULL AUTO_INCREMENT, `task` VARCHAR(300), `color` VARCHAR(7), `clicked` TINYINT, `id_users` VARCHAR(9), PRIMARY KEY (`id_tasks`))";
+    const query_table = "CREATE TABLE IF NOT EXISTS my_task (`id_tasks` INT NOT NULL AUTO_INCREMENT, `task` VARCHAR(300), `color` VARCHAR(7), `clicked` TINYINT, `id_users` VARCHAR(9), PRIMARY KEY (`id_tasks`))";
     const created = await connect.query(query_table);
 
     res.json(created);
 });
 
 server.get("/create-user-table", async (req: Request, res: Response) => {
-    const query_table = "CREATE TABLE IF NOT EXISTS `my_annotations`.`my_user` (`id_users` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(50), `email` VARCHAR(25), `password` VARCHAR(8), PRIMARY KEY (`id_users`))";
+    const query_table = "CREATE TABLE IF NOT EXISTS my_user (`id_users` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(50), `email` VARCHAR(25), `password` VARCHAR(8), PRIMARY KEY (`id_users`))";
     const created = await connect.query(query_table);
 
     res.json(created);
