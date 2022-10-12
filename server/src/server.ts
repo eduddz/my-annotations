ed3cc4341b5b2f5a439a3e79f5bf6bb9c27f0877
eduddz <esteves-dorta@hotmail.com>
2022-10-12 13:10:57 -0300

    Versão 1.3.2

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ const server = express();
 const connect = connection;
 
 server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
+server.use(express.urlencoded({ extended: true }));
 server.use(cors());
 
 
